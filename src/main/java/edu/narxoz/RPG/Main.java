5fca37bf63b550b332a2cbb7629039f02669b996
Medeu228 <iakubovmedeu@gmail.com>
2026-02-10 17:57:11 +0500

    demo

--- src/main/java/edu/narxoz/RPG/Main.java
+++ src/main/java/edu/narxoz/RPG/Main.java
@@ -1,52 +1,34 @@
 package edu.narxoz.RPG;
 
-import edu.narxoz.RPG.characters.*;
+import edu.narxoz.RPG.characters.Character;
 import edu.narxoz.RPG.factory_method.*;
 import edu.narxoz.RPG.abstract_factory.*;
-import edu.narxoz.RPG.interfaces.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // ===== Factory Method (персонажи) =====
+
         CharacterFactory warriorFactory = new WarriorFactory();
         CharacterFactory mageFactory = new MageFactory();
         CharacterFactory archerFactory = new ArcherFactory();
 
-        Character warrior = warriorFactory.createCharacter();
-        Character mage = mageFactory.createCharacter();
-        Character archer = archerFactory.createCharacter();
+        Character warrior = warriorFactory.create();
+        Character mage = mageFactory.create();
+        Character archer = archerFactory.create();
 
-        // ===== Abstract Factory (экипировка) =====
+
         EquipmentFactory medieval = new MedievalEquipmentFactory();
         EquipmentFactory magic = new MagicEquipmentFactory();
         EquipmentFactory ranger = new RangerEquipmentFactory();
 
-        equip(warrior, medieval);
-        equip(mage, magic);
-        equip(archer, ranger);
+        warrior.equip(medieval.createWeapon(), medieval.createArmor());
+        mage.equip(magic.createWeapon(), magic.createArmor());
+        archer.equip(ranger.createWeapon(), ranger.createArmor());
 
-        // ===== Вывод =====
-        show(warrior);
-        show(mage);
-        show(archer);
-    }
 
-    private static void equip(Character character, EquipmentFactory factory) {
-        Weapon weapon = factory.createWeapon();
-        Armor armor = factory.createArmor();
-
-        character.setWeapon(weapon);
-        character.setArmor(armor);
-    }
-
-    private static void show(Character character) {
-        System.out.println("Class: " + character.getName());
-        System.out.println("Weapon: " + character.getWeapon().getName()
-                + " | Damage: " + character.getWeapon().getDamage());
-        System.out.println("Armor: " + character.getArmor().getName()
-                + " | Defense: " + character.getArmor().getDefense());
-        System.out.println("---------------------");
+        warrior.showInfo();
+        mage.showInfo();
+        archer.showInfo();
     }
 }
